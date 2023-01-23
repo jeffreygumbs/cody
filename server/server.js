4aa1d22255a808c2c42ff86c54f99357280808d7
Jeffrey Gumbs <jeffreygumbs@gmail.com>
2023-01-23 12:56:37 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import {Configuration, OpenAIApi} from 'openai';
+
+dotenv.config();
+const port = process.env.PORT || 8080;
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'hello from cody',
+  });
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({error});
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port http://localhost:${port}`);
+});
