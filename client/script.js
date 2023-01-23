134d9db79164117bccded7178d8412e258f2f6bd
Jeffrey Gumbs <jeffreygumbs@gmail.com>
2023-01-23 13:41:29 -0700

    update fetch information

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:8080/', {
+  const response = await fetch('https://cody-778x.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
